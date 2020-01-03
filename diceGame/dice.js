fc9918f29a4fe9a0a686b4bd4a5ae80a0fc65299
Bobby Karabibakis <bkarabinakis@gmail.com>
2020-01-02 17:32:54 -0800

    New Functionality: Scores resets when user rolls 6 twice

--- diceGame/dice.js
+++ diceGame/dice.js
@@ -12,73 +12,88 @@
  * 
  */
 
-var roundScore, globalScore, scores, activePlayer, dice;
+var roundScore, globalScore, scores, activePlayer, dice, gamePlaying, previousRoll, rollNum;
 
 init();
 
+
+
 document.querySelector('.btn-roll').addEventListener("click", function () {
-    //1. Get the random number
-    dice = Math.floor(Math.random() * 6) + 1;
+    //Save previous roll to check if user rolls 6 twice
+    previousRoll = dice;
 
-    //2. Display the number
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
+    if (gamePlaying) {
 
-    diceDOM.src = 'dice-' + dice + '.png';
+        //1. Get the random number
+        dice = Math.floor(Math.random() * 6) + 1;
 
-    //3. Update the score
+        //2. Display the number
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
 
-    if (dice !== 1) {
-        //Add the score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        //Reset the Global Score to 0 when rolling 1 
-        scores[activePlayer] = 0;
-        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        diceDOM.src = 'dice-' + dice + '.png';
 
-        //Switch players and handle the UI highlight
-        nextPlayer();
+        //3. Update the score
 
+        if (!(dice == 1 || dice + previousRoll == 12)) {
+            //Add the score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Reset the Global Score to 0 when rolling 1 
+            scores[activePlayer] = 0;
+            document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-        document.getElementById('current-0').textContent = 0
-        document.getElementById('current-1').textContent = 0
-        document
-
+            //Switch players and handle the UI highlight
+            nextPlayer();
 
 
-        //Hide the Dice when rolling a 1
-        // document.querySelector('.dice').style.display='none';
+            document.getElementById('current-0').textContent = 0
+            document.getElementById('current-1').textContent = 0
+            document
 
+
+
+            //Hide the Dice when rolling a 1
+            // document.querySelector('.dice').style.display='none';
+
+
+        }
 
     }
+
 });
 
 document.querySelector('.btn-hold').addEventListener("click", function () {
-    //Take the 'current' score and add it to the Global score
+    if (gamePlaying) {
+        //Take the 'current' score and add it to the Global score
 
-    scores[activePlayer] += roundScore;
+        scores[activePlayer] += roundScore;
 
-    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-    //Check if the player has won and switch players if not
-    winnerCheck();
+        //Check if the player has won and switch players if not
+        winnerCheck();
 
 
-    //RESET the Current Score after holding
-    document.getElementById('current-' + activePlayer).textContent = 0
+        //RESET the Current Score after holding
+        document.getElementById('current-' + activePlayer).textContent = 0
 
+
+
+    }
 
 });
 
 document.querySelector('.btn-new').addEventListener("click", init);
 
-
+//This function starts the game
 function init() {
     scores = [0, 0];
     roundScore = 0;
     globalScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
 
     //Set all numbers to 0 when starting
     document.getElementById('current-0').textContent = 0
@@ -101,6 +116,7 @@ function init() {
 
 }
 
+//Function that handles the switching of players
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
@@ -110,16 +126,19 @@ function nextPlayer() {
     playerPanelSwitch();
 }
 
+//Handles the UI switch for the CSS while playing
 function playerPanelSwitch() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 }
 
+//Function that checks which player has won
 function winnerCheck() {
     if (scores[activePlayer] >= 50) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        gamePlaying = false;
 
     } else {
         nextPlayer();
